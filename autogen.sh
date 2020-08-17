466a079a1a1e0c71a2cbb9cfa47922b65af8ff47
Aurelien Jarno <aurelien@aurel32.net>
2020-08-17 21:04:23 +0200

    New upstream version 3.1

--- autogen.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh -x
-
-rm -fv ltmain.sh config.sub config.guess config.h.in aclocal.m4
-aclocal -I m4
-autoheader
-libtoolize --automake --copy
-automake --add-missing --copy --force
-autoreconf
-chmod 755 configure
